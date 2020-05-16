9256bc41523102a24b3a7b7166f1d7708afda786
Caio Guimarães <c.ribeiro.guimaraes@accenture.com>
2020-05-16 02:18:34 -0300

    feat: implementar rota para obter o perfil do usuário atual

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,28 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
 
-// @route   GET api/profile
-// @desc    Rota de teste
-// @access  Public
-router.get("/", (req, res) => res.send("Rota do perfil usuário"));
+const User = require("../../models/User");
+const Profile = require("../../models/Profile");
+
+// @route   GET api/profile/me
+// @desc    Obter perfil do usuário atual
+// @access  Private
+router.get("/me", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.user.id,
+    }).populate("user", ["nome", "avatar"]);
+
+    if (!profile) {
+      return res.status(400).json({ msg: "Este usuário não possui perfil" });
+    }
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Erro no servidor");
+  }
+});
 
 module.exports = router;
