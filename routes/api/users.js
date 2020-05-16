b29363540027d620668ce84226965d9588084bc2
Caio Guimarães <c.ribeiro.guimaraes@accenture.com>
2020-05-16 01:00:55 -0300

    feat: implementar jwt token na criação do usuário

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 const { check, validationResult } = require("express-validator");
 
 const User = require("../../models/User");
@@ -49,7 +51,25 @@ router.post(
 
       await user.save();
 
-      res.send("Usuário cadastrado");
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 1300 },
+        async (err, token) => {
+          if (err) throw err;
+          user.token = token;
+          user.data_atualizacao = Date.now();
+          await user.save();
+
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send("Erro no servidor");
