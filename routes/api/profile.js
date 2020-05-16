a527e56111de1d5a969dbda715c5baeb0fd8aa1f
Caio Guimarães <c.ribeiro.guimaraes@accenture.com>
2020-05-16 02:52:21 -0300

    feat: implementar rota para criar/atualizar o perfil do usuário

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const auth = require("../../middleware/auth");
+const { check, validationResult } = require("express-validator");
 
 const User = require("../../models/User");
 const Profile = require("../../models/Profile");
@@ -24,5 +25,54 @@ router.get("/me", auth, async (req, res) => {
     res.status(500).send("Erro no servidor");
   }
 });
+
+// @route   POST api/profile
+// @desc    Criar ou atualizar o perfil do usuário
+// @access  Private
+router.post(
+  "/",
+  [
+    auth,
+    [check("telefones", "Lista de telefones obrigatória").not().isEmpty()],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { telefones } = req.body;
+
+    const profileFields = {};
+
+    profileFields.user = req.user.id;
+
+    if (telefones) profileFields.telefones = telefones;
+
+    try {
+      let profile = await Profile.findOne({ user: req.user.id });
+
+      if (profile) {
+        profileFields.data_atualizacao = Date.now();
+
+        profile = await Profile.findOneAndUpdate(
+          { user: req.user.id },
+          { $set: profileFields },
+          { new: true }
+        );
+
+        return res.json(profile);
+      }
+
+      profile = new Profile(profileFields);
+      await profile.save();
+
+      res.json(profile);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Erro no servidor");
+    }
+  }
+);
 
 module.exports = router;
