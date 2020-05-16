8c9dab53ce9a933852e52c65e92d7e0b84d85eaa
Caio Guimarães <c.ribeiro.guimaraes@accenture.com>
2020-05-16 01:24:17 -0300

    feat: implementar decodificação do token

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,20 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
+
+const User = require("../../models/User");
 
 // @route   GET api/auth
 // @desc    Rota de teste
 // @access  Public
-router.get("/", (req, res) => res.send("Rota de autenticação"));
+router.get("/", auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-senha");
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Erro no servidor");
+  }
+});
 
 module.exports = router;
